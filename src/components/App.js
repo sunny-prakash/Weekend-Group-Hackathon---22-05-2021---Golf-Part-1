944c46d29ae55395ca7d982bbb29232d4cbda1ff
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-22 20:18:31 +0530

    ball just moving 5 px right

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,41 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
+            posi: 0,
+            ballPosition: { left: "0px" },
         };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
+        this.renderChoice = this.renderBallOrButton.bind(this);
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    }
 
     buttonClickHandler() {
-   
-   }
+        this.setState({ renderBall: true });
+    }
     renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+        if (this.state.renderBall) {
+            return <div className="ball" style={this.state.ballPosition}></div>;
+        } else {
+            return <button onClick={this.buttonClickHandler}>Click For One Ball</button>;
+        }
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "ArrowRight") {
+                this.setState({ ballPosition: { left: "5px" } });
+            }
+        });
     }
 
     render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+        return <div className="playground">{this.renderBallOrButton()}</div>;
     }
 }
-
 
 export default App;
